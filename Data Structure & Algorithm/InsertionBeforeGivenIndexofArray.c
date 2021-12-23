55fa531deb63d80fac6c8ea7f4151c90ee83cc04
ais95 <arifalislam642@gmail.com>
2021-12-23 14:11:04 +0600

    ds & algo test

---
+++ Data Structure & Algorithm/InsertionBeforeGivenIndexofArray.c
@@ -0,0 +1,54 @@
+/* begin
+
+IF N = MAX, return
+ELSE
+   N = N + 1
+
+   SEEK Location index
+
+   For All Elements from A[index - 1] to A[N]
+      Move to next adjacent location
+
+   A[index - 1] = New_Element
+
+end   */
+
+// ----------- IMPLEMENTATION ------------
+
+#include <stdio.h>
+
+#define MAX 5
+
+void main() {
+   int array[MAX] = {1, 2, 4, 5};
+
+   int N = 4;        // number of elements in array
+   int i = 0;        // loop variable
+   int index = 3;    // index location before which value will be inserted
+   int value = 3;    // new data element to be inserted
+
+   // print array before insertion
+   printf("Printing array before insertion \n");
+
+   for(i = 0; i < N; i++) {
+      printf("array[%d] = %d \n", i, array[i]);
+   }
+
+   // now shift rest of the elements downwards
+   for(i = N; i >= index + 1; i--) {
+      array[i + 1] = array[i];
+   }
+
+   // add new element at first position
+   array[index + 1] = value;
+
+   // increase N to reflect number of elements
+   N++;
+
+   // print to confirm
+   printf("Printing array after insertion \n");
+
+   for(i = 0; i < N; i++) {
+      printf("array[%d] = %d\n", i, array[i]);
+   }
+}
